eb2fef9b7384f9711dd848af49cf9efa8625d0b7
KyuoFoxHuyu <88403766@qq.com>
2021-05-29 20:43:11 +0800

    regen vendorsetup.sh
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,50 +34,50 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-    export PLATFORM_VERSION="16.1.0"
-   	export PLATFORM_SECURITY_PATCH="2099-12-31"
-   	export TW_DEFAULT_LANGUAGE="en"
+    export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+ 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+ 	export TARGET_DEVICE_ALT="lmi"
+	
 	export OF_KEEP_FORCED_ENCRYPTION=1
 	export OF_PATCH_AVB20=1
-    export OF_SCREEN_H=2400
-    export OF_STATUS_INDENT_LEFT=150
-    export OF_STATUS_INDENT_RIGHT=48
-    export OF_STATUS_H=115
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1	
+ 
 	export OF_USE_MAGISKBOOT=1
+	export OF_USE_NEW_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
+	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
 	export OF_NO_MIUI_PATCH_WARNING=1
-
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export FOX_REPLACE_BUSYBOX_PS=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_USE_NEW_MAGISKBOOT=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 	export FOX_DELETE_AROMAFM=1
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
 
     # use system (ROM) fingerprint where available
     export OF_USE_SYSTEM_FINGERPRINT=0
 
-	# OTA for custom ROMs
+	# OTA
+	export OF_KEEP_DM_VERITY=1
     export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
     export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+         
+    # Screen Settings
+    export OF_SCREEN_H=2400
+    export OF_STATUS_INDENT_LEFT=150
+    export OF_STATUS_INDENT_RIGHT=48
+    export OF_STATUS_H=115
 
     # -- add settings for R11 --
-    export FOX_VERSION=R11.0_0
+    export FOX_VERSION=R11.1_0
     export FOX_BUILD_TYPE=Beta
-    export OF_MAINTAINER=KyuoFoxHuyu
-    export FOX_R11=1
-    export FOX_ADVANCED_SECURITY=1
+    export OF_MAINTAINER=Kyuofox
     export OF_USE_TWRP_SAR_DETECT=1
-    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-    export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
-    export FOX_INSTALLER_DEBUG_MODE=1
     # -- end R11 settings --
 
 	# let's see what are our build VARs
